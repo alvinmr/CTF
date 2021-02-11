599a6214e48bb915f73d69d2a4bf41c35ba679e1
alvinmr <maulanaalvin1@gmail.com>
2021-02-11 15:45:01 +0800

    fixing print not display

--- Reverse/ezpz/rev1.c
+++ Reverse/ezpz/rev1.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    signed int pin;
+void ignore_me_init_buffering() {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+}
 
-    printf("%s","Masukkan PIN: ");
+int main(int argc, char* argv[]){
+    ignore_me_init_buffering();
+    signed int pin;
+    
+    printf("Masukkan PIN mu : ");
     scanf("%d",&pin);
     
     if(pin == 65535){
